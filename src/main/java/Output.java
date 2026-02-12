256a1bc75983dc2ebe1dc77f192c766d49f23d7f
koomin1227 <koomin1227@naver.com>
2026-02-12 15:18:27 +0900

    step2 구현

--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -1,8 +1,8 @@
 public class Output {
 
     // 입력 받은 금액으로 로또 몇 개 구매했고, 구매된 로또들의 각 번호 출력
-    public static void printLottos(Lottos lottos) {
-        System.out.printf("%d개를 구매했습니다.\n", lottos.size());
+    public static void printLottos(Lottos lottos, Integer manualLottoCount) {
+        System.out.printf("수동으로 %d장, 자동으로 %d개를 구매했습니다.\n", lottos.size(), manualLottoCount);
         for (Lotto lotto : lottos.getLottos()) {
             System.out.println(lotto);
         }
